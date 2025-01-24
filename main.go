cc98be31aad8099a2e4f4a1d3db6eb7325ab5691
Baris Dogu <barisdogu@outlook.de>
2025-01-24 21:47:04 +0300

    #build #release

--- main.go
+++ main.go
@@ -5,16 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/fatih/color"
-	"golang.org/x/term"
-
 	u "github.com/bariiss/aws-mfa-go/util"
+	"github.com/fatih/color"
 )
 
 var (
@@ -23,7 +20,7 @@ var (
 )
 
 func main() {
-	if checkProfile() {
+	if u.CheckProfile(awsProfile, awsRegion) {
 		return
 	}
 
@@ -36,15 +33,15 @@ func main() {
 		log.Fatalf("\nUnable to load AWS config: %s", err)
 	}
 
-	credentialsFilePath := getCredentialsFilePath()
+	credentialsFilePath := u.GetCredentialsFilePath()
 	expirationTime, err := u.ReadExpirationTime(credentialsFilePath, awsProfile)
 
 	if err != nil {
 		log.Printf("\nError reading expiration time: %s", err)
 	}
 
-	if expirationTimeValid(expirationTime) {
-		if !confirmContinuation() {
+	if u.ExpirationTimeValid(awsProfile, expirationTime) {
+		if !u.ConfirmContinuation() {
 			fmt.Println("\n🤖 Operation aborted.")
 			return
 		}
@@ -94,78 +91,6 @@ func main() {
 		log.Fatalf("\nError saving credentials to file: %s", err)
 	}
 
-	printInfo()
-}
-
-func checkProfile() bool {
-	if awsProfile == "" || awsRegion == "" {
-		color.Red("🚨 Missing environment variables. AWS_MFA_GO_USER and AWS_MFA_GO_REGION are required.")
-		color.Yellow("👉 Set these variables to continue. Example:")
-		color.Yellow("   export AWS_MFA_GO_USER=<profile>")
-		color.Yellow("   export AWS_MFA_GO_REGION=<region>")
-		color.Yellow("\n👉 Save your AWS credentials in the credentials file with a -go suffix:\n   [<profile>-go]\n   aws_access_key_id = <aws_access_key_id>\n   aws_secret_access_key = <aws_secret_access_key>\n   aws_mfa_device = <aws_mfa_device>\n   aws_mfa_duration = <aws_mfa_duration>\n   aws_mfa_secret_key = <aws_mfa_secret_key>")
-		return true
-	}
-	return false
-}
-
-func getCredentialsFilePath() string {
-	homeDirs, err := os.UserHomeDir()
-
-	if err != nil {
-		log.Fatalf("\nError getting home directory: %s", err)
-	}
-
-	return filepath.Join(homeDirs, ".aws", "credentials")
-}
-
-func expirationTimeValid(expirationTime time.Time) bool {
-	if expirationTime.IsZero() {
-		fmt.Printf("\nProfile '%s' does not exist, skipping expiration check.\n", awsProfile)
-		return false
-	}
-
-	if time.Now().Before(expirationTime) {
-		remainingDuration := expirationTime.Sub(time.Now().UTC()).Hours()
-		fmt.Printf("📌 The current token for profile '%s' is valid until %v (about %.2f hours remaining).\n", awsProfile, expirationTime.Format("2006-01-02 15:04:05"), remainingDuration)
-		return true
-	}
-
-	return false
-}
-
-func confirmContinuation() bool {
-	fmt.Print("🤔 Do you want to continue and generate a new token? (y/n): ")
-
-	// Set the terminal to raw mode to capture a single keystroke
-	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
-
-	if err != nil {
-		fmt.Printf("\nError setting terminal to raw mode: %s\n", err)
-		return false
-	}
-
-	defer func(fd int, oldState *term.State) {
-		err := term.Restore(fd, oldState)
-		if err != nil {
-			fmt.Printf("\nError restoring terminal state: %s\n", err)
-		}
-	}(int(os.Stdin.Fd()), oldState)
-
-	// Read a single character
-	var response = make([]byte, 1)
-	_, err = os.Stdin.Read(response)
-
-	if err != nil {
-		fmt.Printf("\nError reading response: %s\n", err)
-		return false
-	}
-
-	// Check if the response is "y"
-	return response[0] == 'y'
-}
-
-func printInfo() {
 	fmt.Printf("\n👍 Credentials saved to file for profile '%s'.\n🎉 You can now use profile '%s' with the AWS CLI.\n", awsProfile, awsProfile)
 	color.Yellow("👉 Example: aws configure --profile %s", awsProfile)
 	color.Cyan("👌 Example: aws --profile %s s3 ls", awsProfile)
--- util/credentials.go
+++ util/credentials.go
@@ -3,7 +3,11 @@ package util
 import (
 	"bufio"
 	"fmt"
+	"github.com/fatih/color"
+	"golang.org/x/term"
+	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -27,6 +31,101 @@ func SaveCredentialsToFile(f string, p string, c *Credentials) error {
 	}
 
 	return writeLinesToFile(f, lines)
+}
+
+func ReadExpirationTime(filePath, profile string) (time.Time, error) {
+	lines, err := readLines(filePath)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	profileHeader := "[" + profile + "]"
+	inProfile := false
+
+	for _, line := range lines {
+		if strings.TrimSpace(line) == profileHeader {
+			inProfile = true
+			continue
+		}
+		if inProfile && strings.HasPrefix(line, "[") {
+			break
+		}
+		if inProfile && strings.Contains(line, "expiration =") {
+			expirationStr := strings.SplitN(line, "=", 2)[1]
+			expirationStr = strings.TrimSpace(expirationStr)
+			return time.Parse("2006-01-02 15:04:05", expirationStr)
+		}
+	}
+
+	return time.Time{}, fmt.Errorf("\nExpiration time not found for profile %s", profile)
+}
+
+func CheckProfile(awsProfile string, awsRegion string) bool {
+	if awsProfile == "" || awsRegion == "" {
+		color.Red("🚨 Missing environment variables. AWS_MFA_GO_USER and AWS_MFA_GO_REGION are required.")
+		color.Yellow("👉 Set these variables to continue. Example:")
+		color.Yellow("   export AWS_MFA_GO_USER=<profile>")
+		color.Yellow("   export AWS_MFA_GO_REGION=<region>")
+		color.Yellow("\n👉 Save your AWS credentials in the credentials file with a -go suffix:\n   [<profile>-go]\n   aws_access_key_id = <aws_access_key_id>\n   aws_secret_access_key = <aws_secret_access_key>\n   aws_mfa_device = <aws_mfa_device>\n   aws_mfa_duration = <aws_mfa_duration>\n   aws_mfa_secret_key = <aws_mfa_secret_key>")
+		return true
+	}
+	return false
+}
+
+func GetCredentialsFilePath() string {
+	homeDirs, err := os.UserHomeDir()
+
+	if err != nil {
+		log.Fatalf("\nError getting home directory: %s", err)
+	}
+
+	return filepath.Join(homeDirs, ".aws", "credentials")
+}
+
+func ExpirationTimeValid(awsProfile string, expirationTime time.Time) bool {
+	if expirationTime.IsZero() {
+		fmt.Printf("\nProfile '%s' does not exist, skipping expiration check.\n", awsProfile)
+		return false
+	}
+
+	if time.Now().Before(expirationTime) {
+		remainingDuration := expirationTime.Sub(time.Now().UTC()).Hours()
+		fmt.Printf("📌 The current token for profile '%s' is valid until %v (about %.2f hours remaining).\n", awsProfile, expirationTime.Format("2006-01-02 15:04:05"), remainingDuration)
+		return true
+	}
+
+	return false
+}
+
+func ConfirmContinuation() bool {
+	fmt.Print("🤔 Do you want to continue and generate a new token? (y/n): ")
+
+	// Set the terminal to raw mode to capture a single keystroke
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+
+	if err != nil {
+		fmt.Printf("\nError setting terminal to raw mode: %s\n", err)
+		return false
+	}
+
+	defer func(fd int, oldState *term.State) {
+		err := term.Restore(fd, oldState)
+		if err != nil {
+			fmt.Printf("\nError restoring terminal state: %s\n", err)
+		}
+	}(int(os.Stdin.Fd()), oldState)
+
+	// Read a single character
+	var response = make([]byte, 1)
+	_, err = os.Stdin.Read(response)
+
+	if err != nil {
+		fmt.Printf("\nError reading response: %s\n", err)
+		return false
+	}
+
+	// Check if the response is "y"
+	return response[0] == 'y'
 }
 
 func readLines(filePath string) ([]string, error) {
@@ -120,31 +219,4 @@ func writeLinesToFile(filePath string, lines []string) error {
 		}
 	}
 	return nil
-}
-
-func ReadExpirationTime(filePath, profile string) (time.Time, error) {
-	lines, err := readLines(filePath)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	profileHeader := "[" + profile + "]"
-	inProfile := false
-
-	for _, line := range lines {
-		if strings.TrimSpace(line) == profileHeader {
-			inProfile = true
-			continue
-		}
-		if inProfile && strings.HasPrefix(line, "[") {
-			break
-		}
-		if inProfile && strings.Contains(line, "expiration =") {
-			expirationStr := strings.SplitN(line, "=", 2)[1]
-			expirationStr = strings.TrimSpace(expirationStr)
-			return time.Parse("2006-01-02 15:04:05", expirationStr)
-		}
-	}
-
-	return time.Time{}, fmt.Errorf("\nExpiration time not found for profile %s", profile)
 }
